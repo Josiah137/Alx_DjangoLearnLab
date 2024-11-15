80b87e5bda9d17442a8640cb5423ef9984927aae
Your Name <eyosi50@gmail.com>
2024-11-15 14:14:31 +0300

    the checker could not find my repo so i am making some changes. i put django-modles/LibraryProject then all the files in that.... if that can work

---
+++ django-models/LibraryProject/bookshelf/admin.py
@@ -0,0 +1,21 @@
+from django.contrib import admin
+
+# Register your models here. 
+from .models import Book  
+
+# Register the Book model with the admin  
+# admin.site.register(Book)
+
+
+class BookAdmin(admin.ModelAdmin):  
+    # Specify the fields to display in the list view  
+    list_display = ('title', 'author', 'publication_year')  
+    
+    # Add filters for publication year  
+    list_filter = ('publication_year',)  
+    
+    # Enable search functionality on title and author  
+    search_fields = ('title', 'author__name')  
+
+# Register the Book model with the custom admin class  
+admin.site.register(Book, BookAdmin)
