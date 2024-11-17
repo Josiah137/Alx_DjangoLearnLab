f0fd4e6379f1d5c23932eeefb8333ddab54473f8
Your Name <eyosi50@gmail.com>
2024-11-18 00:04:37 +0300

    task 2

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,3 +1,21 @@
 from django.shortcuts import render
 
 # Create your views here.
+from django.shortcuts import render  
+from .models import Book  # Make sure to import your Book model  
+
+def list_books(request):  
+    books = Book.objects.all()  # Retrieve all books from the database  
+    return render(request, 'list_books.html', {'books': books})  # Render the template with books
+
+
+from django.views.generic import DetailView  
+from .models import Library  # Import your Library model  
+
+class LibraryDetailView(DetailView):  
+    model = Library  # Specify the model to use  
+    template_name = 'library_detail.html'  # Specify the template to render  
+    context_object_name = 'library'  # Name of the context object to use in the template  
+
+    def get_queryset(self):  
+        return Library.objects.prefetch_related('books')  # Prefetch related books for efficiency
