d79102fc743d3169d63f89dbc316de8031e37b45
Your Name <eyosi50@gmail.com>
2024-11-20 14:37:39 +0300

    Checks for “Query all books by a specific author.”... i just modified the parameter

--- django-models/LibraryProject/relationship_app/query_samples.py
+++ django-models/LibraryProject/relationship_app/query_samples.py
@@ -13,9 +13,9 @@ django.setup()
 from relationship_app.models import Author, Book, Library, Librarian
 
 # 1. Query all books by a specific author
-def get_books_by_author(author_name):
+def get_books_by_author(author):
     try:
-        author = Author.objects.get(name=author_name)
+        author = Author.objects.get(name=author)
         books = author.books.all()  # Using the related name "books"
         return books
     except Author.DoesNotExist:
