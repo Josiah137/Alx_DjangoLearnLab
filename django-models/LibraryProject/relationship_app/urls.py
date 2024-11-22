1f289a956591cc5567ae580ff767c9cfd143e232
Your Name <eyosi50@gmail.com>
2024-11-22 19:49:04 +0300

    task is not working for some reason

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -20,7 +20,7 @@ urlpatterns = [
     path("", views.home, name="home"),  # Home page URL
 
     # urls for displaying role based views
-    path('admin/', views.admin_view, name='admin_view'),
+    path('admin/', views.admin_view, name='admin_view'), 
     path('librarian/', views.librarian_view, name='librarian_view'),
     path('member/', views.member_view, name='member_view'),
 ]
