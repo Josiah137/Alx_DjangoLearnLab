96ed845e330342013cbfd47c2891c3d974f25633
Your Name <eyosi50@gmail.com>
2024-12-02 13:55:35 +0300

    trying serilization, and qurey

--- my_trails/week12/my_project/my_week12_app/Serlializers.py
+++ my_trails/week12/my_project/my_week12_app/Serlializers.py
@@ -5,9 +5,9 @@ from datetime import date  # Import the date class
 class BookSerializer(serializers.ModelSerializer):
     class Meta:
         model = Book
-        fields = '__all__'
+        fields = '__all__' # this is to make all list to be displayed on the fields (you can also list speific fields)
 
-    # Define the custom field to be calculated
+    # Define the custom field to be calculated .......... to add new feilds i think. and on the name of the fieild there will be  a method
     days_since_creation = serializers.SerializerMethodField()
         # This method calculates the number of days since creation
     def get_days_since_creation(self, obj):
--- my_trails/week12/my_project/my_week12_app/views.py
+++ my_trails/week12/my_project/my_week12_app/views.py
@@ -8,4 +8,21 @@ from .Serlializers import BookSerializer
 
 class BookListCreateAPIView(generics.ListCreateAPIView):
     queryset = Book.objects.all()  # Fetch all books from the database
-    serializer_class = BookSerializer  # Use the BookSerializer for this view
+    serializer_class = BookSerializer  # Use the BookSerializer for this view (defined in the serializer.py  file)
+
+    def get_queryset(self):
+        # Get all books by default
+        queryset = self.queryset
+         # Allow filtering by title
+        title = self.request.query_params.get('title', None) #... this return none if it can not get the requested title 
+        if title: # in other words, if it is not none .....    `if title is not None:` also works 
+            #filtering with the requested title name from our queryset(all the books)
+            queryset = queryset.filter(title__icontains=title) 
+
+        # Filter by author (optional)
+        author = self.request.query_params.get('author', None)
+        if author:
+            queryset = queryset.filter(author__icontains=author)
+
+        return queryset
+    
