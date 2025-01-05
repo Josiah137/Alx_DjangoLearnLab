beae232875bac87db902ff9d51980fca10f970ab
Your Name <eyosi50@gmail.com>
2025-01-06 00:50:46 +0300

    modifying the view.py so that certain viesset will be seen to authticated users only

--- my_trails/week12/Library_system/lib_api/views.py
+++ my_trails/week12/Library_system/lib_api/views.py
@@ -6,12 +6,21 @@ from rest_framework.viewsets import ModelViewSet
 from .models import Book, Author, Genre
 from .Serializator import AuthorSerializer, GenreSerializers, BookSerializers
 from rest_framework.permissions import IsAdminUser, IsAuthenticated
+from rest_framework.response import Response
+# from rest_framework.authentication import TokenAuthentication
+from rest_framework.authentication import BasicAuthentication
 
 #settting app all instances and their respective serilizer
 class Bookviewset(ModelViewSet):
     queryset = Book.objects.all()
     serializer_class = BookSerializers
-    # permission_classes = [IsAuthenticated]
+    authentication_classes = [BasicAuthentication]
+    permission_classes = [IsAuthenticated] # this line set the permission for the view to be seen by authticated users only and we set
+                                           # what kinda of authtication we meant on z above, basic Auth. it can also be other type like token
+                                         
+
+    def get(self, request):
+        return Response({"message": "You have access!"})
 
 
 class AuthorViewSet(ModelViewSet):
