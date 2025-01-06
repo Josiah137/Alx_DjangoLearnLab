6e1f0f9f93085fb7ac3a66e093a4e8fbfebdb9cd
Your Name <eyosi50@gmail.com>
2025-01-06 14:32:33 +0300

    modifying views to set diffrent variations like to setting diffrent resticition for diffrent views

--- my_trails/final_project_perp/Library_system/lib_api/views.py
+++ my_trails/final_project_perp/Library_system/lib_api/views.py
@@ -5,18 +5,19 @@ from django.shortcuts import render
 from rest_framework.viewsets import ModelViewSet
 from .models import Book, Author, Genre
 from .Serializator import AuthorSerializer, GenreSerializers, BookSerializers
-from rest_framework.permissions import IsAdminUser, IsAuthenticated
+from rest_framework.permissions import IsAdminUser, IsAuthenticated, IsAuthenticatedOrReadOnly
 from rest_framework.response import Response
-from rest_framework.authentication import TokenAuthentication
-from rest_framework.authentication import BasicAuthentication #.... this is for simple autication user and pass in week 12
+# from rest_framework.authentication import TokenAuthentication
+from rest_framework.authentication import BasicAuthentication
 
 #settting app all instances and their respective serilizer
+# permission class is the one that block users from acesssing certain content if we reove that everyone has acess for everything. so sensetive contents has to be protected
 class Bookviewset(ModelViewSet):
     queryset = Book.objects.all()
     serializer_class = BookSerializers
     authentication_classes = [BasicAuthentication]
     permission_classes = [IsAuthenticated] # this line set the permission for the view to be seen by authticated users only and we set
-                                           # what kinda of authtication we meant on z above, basic Auth. it can also be other type like token
+                                           # what kinda of authtication we meant on z above, basic Auth. it can also be other type like token auth
                                          
 
     def get(self, request):
@@ -26,7 +27,8 @@ class Bookviewset(ModelViewSet):
 class AuthorViewSet(ModelViewSet):
     queryset = Author.objects.all()
     serializer_class = AuthorSerializer
-
+    permission_classes = [IsAuthenticated, IsAdminUser] # only admins can acsses this now(get, post )
 class GenreViewSet(ModelViewSet):
     queryset = Genre.objects.all()
     serializer_class = GenreSerializers
+    permission_classes = [IsAuthenticatedOrReadOnly] # we made it like users can add teh gener of a book only if they are authic/knowen user(loged in)
